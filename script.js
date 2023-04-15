fb721f2c95ce6d9855d81bb14a1a1cf09c11f0c8
dhanraj-saur <121377364+dhanraj-saur@users.noreply.github.com>
2023-04-15 20:23:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //your JS code here. If required.
 function mybutton(){
-	alert("kjsdhkj")
+	
+	const myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age >= 18) {
+				
+                resolve({
+					alert("kjsdhkj")
+
+                });
+            }
+            else {
+                reject("User Name is not correct")
+            }
+        },0);
+    });
+
 }
